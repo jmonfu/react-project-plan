bf0c769dd4d818c72d69200daba3135fa2d13748
Johann Montfort <jmonfu@gmail.com>
2019-07-18 14:54:44 +0200

    store enhancers & prepping for insert in Firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCjNJKLmNXQMocrp1B83IB9BzeMDJs-8nk",
+    authDomain: "react-project-plan-59fc2.firebaseapp.com",
+    databaseURL: "https://react-project-plan-59fc2.firebaseio.com",
+    projectId: "react-project-plan-59fc2",
+    storageBucket: "",
+    messagingSenderId: "406529987015",
+    appId: "1:406529987015:web:17350d1282add249"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({timestampsInSnapshots: true })
+
+  export default firebase;
